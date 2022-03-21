5222beea8f23375b89668a289c3d01974cefdaf6
gwx <13476702487@163.com>
2022-03-21 11:49:38 +0800

    '提交'

--- run.py
+++ run.py
@@ -5,9 +5,7 @@ if __name__ == '__main__':
     # run test file
     # seldom.main("./test_dir/test_sample.py")
     # run test dir
-    # seldom.main("./test_dir/test_encryption.py")
+    seldom.main("./test_dir/test_encryption.py")
     # seldom.main("./test_dir/")
-    en = EncryptionPage()
-    data = en.get_data_key(1)
-    print(data)
+
 
